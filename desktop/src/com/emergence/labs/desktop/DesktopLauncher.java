8e09a27b742e721aa54dadc4b41a2f48d05564fd
Carlos Domingues <c.domingues@techfirm.pt>
2020-03-21 19:14:25 +0000

    swapped Lwjgl  for  Lwjgl3

--- desktop/src/com/emergence/labs/desktop/DesktopLauncher.java
+++ desktop/src/com/emergence/labs/desktop/DesktopLauncher.java
@@ -2,13 +2,22 @@ package com.emergence.labs.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.emergence.labs.GDXMain;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.emergence.labs.GameLifeCycle;
 
 public class DesktopLauncher {
 
+	private static final String TITLE_USED_AT_WINDOW_TOP_BAR = "Bye World!";
+	private static final int WINDOW_WIDTH = 480;
+	private static final int WINDOW_HEIGHT = 320;
+
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new GDXMain(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle(TITLE_USED_AT_WINDOW_TOP_BAR);
+		config.setWindowedMode(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+		new Lwjgl3Application(new GameLifeCycle(), config);
 	}
 
 }
